00d058de3f5b2e7957e9c8097c57d8a0850f88c1
Sachindeep singh <sachindeep.redsky@gmail.com>
2026-09-19 15:27:14 +0530

    fixed ui

--- app/manifest.ts
+++ app/manifest.ts
@@ -11,8 +11,8 @@ export default function manifest(): MetadataRoute.Manifest {
     theme_color: '#071238',
     orientation: 'portrait',
     icons: [
-      { src: '/icon-light-32x32.png', sizes: '32x32', type: 'image/png' },
-      { src: '/apple-icon.png', sizes: '180x180', type: 'image/png' },
+      { src: '/apple-icon.svg', sizes: '32x32', type: 'image/svg' },
+      { src: '/apple-icon.svg', sizes: '180x180', type: 'image/svg' },
     ],
   }
 }
